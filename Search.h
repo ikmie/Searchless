051c2b6946b184c6acf06feced2cd45c5c626585
ShortXander101205 <xander.courses.101205@gmail.com>
2025-05-03 17:58:39 +0700

    Create Search.h

---
+++ Search.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include "Initialize.h"
+
+void searchbyID(long long int id);
+void searchbyName(const char *name);
+void searchScoreRange(float min, float max);
+
+#endif
